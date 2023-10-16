51bb5d4085150cd94a1f0b8271d6608ddf537046
Myrroddin <psvander@gmail.com>
2023-10-16 12:15:46 -0600

    [Retail] Print error and exit addon, as it is NOT compatible with retail WoW

--- Hide.lua
+++ Hide.lua
@@ -1,3 +1,8 @@
+if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+    error("HideWOrldMapMinimapButton does NOT work for retail!", 2)
+    return
+end
+
 local name, AddOn = ...
 
 AddOn.frame = CreateFrame("Frame")
@@ -6,6 +11,7 @@ AddOn.frame:SetScript("OnEvent", function(self, event, ...)
     AddOn[event](self, ...)
 end)
 
-function AddOn:PLAYER_ENTERING_WORLD(isInitialLogin, isReloadingUi)
+function AddOn:PLAYER_ENTERING_WORLD(event)
+    self:UnregisterEvent(event)
     _G["MiniMapWorldMapButton"]:Hide()
 end
