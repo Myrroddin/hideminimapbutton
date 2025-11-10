11b3b62e31075fd65a727c3b66349686bb51c116
Myrroddin <psvander@gmail.com>
2025-11-10 04:45:26 -0700

    Bump ToC file

--- Hide.lua
+++ Hide.lua
@@ -1,18 +1,18 @@
 local name, AddOn = ...
 
 if WOW_PROJECT_ID ~= WOW_PROJECT_MISTS_CLASSIC then
-    error(name .. " does NOT work for this game version", 2)
-    return
+	error(name .. " does NOT work for this game version", 2)
+	return
 end
 
 
 AddOn.frame = CreateFrame("Frame")
 AddOn.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 AddOn.frame:SetScript("OnEvent", function(self, event, ...)
-    AddOn[event](self, ...)
+	AddOn[event](self, ...)
 end)
 
 function AddOn:PLAYER_ENTERING_WORLD()
-    AddOn.frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
-    _G["MiniMapWorldMapButton"]:Hide()
+	AddOn.frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	_G["MiniMapWorldMapButton"]:Hide()
 end
