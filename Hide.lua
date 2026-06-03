41b796d0e3ac3aba9d4a6ffa9c78d4f1c083c055
Myrroddin <psvander@gmail.com>
2026-06-03 10:19:04 -0600

    Bump ToC for patch 5.5.4

--- Hide.lua
+++ Hide.lua
@@ -1,18 +1,21 @@
+---@class HideWorldMapMinimapButton
+---@field frame Frame
+
+---@type string, HideWorldMapMinimapButton
 local name, AddOn = ...
 
 if WOW_PROJECT_ID ~= WOW_PROJECT_MISTS_CLASSIC then
-	error(name .. " does NOT work for this game version", 2)
-	return
+	error(name .. " does NOT work for this game version")
 end
-
 
 AddOn.frame = CreateFrame("Frame")
-AddOn.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-AddOn.frame:SetScript("OnEvent", function(self, event, ...)
-	AddOn[event](self, ...)
-end)
 
-function AddOn:PLAYER_ENTERING_WORLD()
-	AddOn.frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	_G["MiniMapWorldMapButton"]:Hide()
-end
+AddOn.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+AddOn.frame:SetScript("OnEvent", function(self)
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
+	local button = _G.MiniMapWorldMapButton
+	if button then
+		button:Hide()
+	end
+end)
